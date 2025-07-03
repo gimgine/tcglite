332c128c6a9a1729bb0d34efc8b81686d61fa059
jreinke428 <93751082+jreinke428@users.noreply.github.com>
2025-07-02 22:25:12 -0500

    finish base functionality of pricing

--- app/src/views/PricingView.vue
+++ app/src/views/PricingView.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="grid grid-cols-12 gap-4">
-    <div class="col-span-12 flex flex-wrap gap-4 rounded-md bg-white p-8 shadow md:col-span-4">
-      <div class="w-full text-sm text-gray-600">Market Price</div>
-      <FloatLabel variant="on">
-        <InputNumber id="floorPrice" v-model="floorPrice" size="small" type="number" />
-        <label for="floorPrice">Floor Price</label>
-      </FloatLabel>
-      <FloatLabel variant="on">
-        <InputNumber id="marketPriceMultiplier" v-model="marketPriceMultiplier" size="small" type="number" />
-        <label for="marketPriceMultiplier">Multiplier</label>
-      </FloatLabel>
-      <div class="flex items-center gap-2">
-        <Checkbox v-model="selected" input-id="selected" size="small" binary />
-        <label for="selected" class="text-sm text-gray-600">Selected</label>
+    <div class="col-span-12 rounded-md bg-white p-8 shadow lg:col-span-4">
+      <span class="w-full text-sm text-gray-600">Market Price</span>
+      <Button class="float-right" label="Update" size="small" :disabled="!pricing.length || !marketPriceMultiplier" @click="updateMarketPrice" />
+      <div class="mt-6 flex gap-2">
+        <FloatLabel variant="on">
+          <InputText id="floorPrice" v-model="floorPrice" size="small" type="number" fluid />
+          <label for="floorPrice">Floor Price</label>
+        </FloatLabel>
+        <FloatLabel variant="on">
+          <InputText id="marketPriceMultiplier" v-model="marketPriceMultiplier" size="small" type="number" fluid />
+          <label for="marketPriceMultiplier">Multiplier</label>
+        </FloatLabel>
+        <div class="flex items-center gap-1">
+          <Checkbox v-model="selected" input-id="selected" size="small" binary />
+          <label for="selected" class="text-xs text-gray-600">Selected</label>
+        </div>
       </div>
-      <Button class="ml-auto" label="Update" :disabled="!pricing.length || !marketPriceMultiplier" @click="updateMarketPrice" />
     </div>
     <div class="col-span-12 rounded-md bg-white p-8 shadow">
       <DataTable
@@ -34,10 +36,9 @@
           <div class="flex items-center gap-2">
             <span class="mr-auto text-lg">Pricing Table</span>
             <FileUpload mode="basic" choose-label="Pricing CSV" choose-icon="pi pi-dollar" accept=".csv" auto @select="handlePricingUpload" />
-            <Button label="Export" icon="pi pi-export" />
+            <Button label="Export" icon="pi pi-file-export" @click="exportPricing" />
           </div>
         </template>
-        <template #empty> Upload the pricing CSV </template>
         <Column selection-mode="multiple" header-style="width: 3rem"></Column>
         <Column field="TCGplayer Id" header="TCGPlayer ID" sortable>
           <template #filter="{ filterModel, filterCallback }">
@@ -127,7 +128,7 @@
 <script setup lang="ts">
 import { parsePricingCsv, type PricingCsv } from '@/util/csv-parse';
 import Papa from 'papaparse';
-import { FileUpload, type FileUploadSelectEvent, DataTable, Column, InputText, FloatLabel, InputNumber, Button, Checkbox } from 'primevue';
+import { FileUpload, type FileUploadSelectEvent, DataTable, Column, InputText, FloatLabel, Button, Checkbox } from 'primevue';
 import { ref } from 'vue';
 import { FilterMatchMode } from '@primevue/core/api';
 
@@ -160,28 +161,17 @@ const handlePricingUpload = async (event: FileUploadSelectEvent) => {
 };
 
 const updateMarketPrice = () => {
-  pricing.value.forEach((product) => {
-    if (product['TCG Marketplace Price'] !== product['TCG Market Price']) {
-      if (product['TCG Market Price'] <= floorPrice.value) {
-        product['TCG Marketplace Price'] = floorPrice.value;
-      } else {
-        product['TCG Marketplace Price'] = product['TCG Market Price'] * marketPriceMultiplier.value;
-      }
+  console.log(selectedRows.value);
+  (selected.value ? selectedRows.value : pricing.value).forEach((product) => {
+    if (product['TCG Market Price'] <= floorPrice.value) {
+      product['TCG Marketplace Price'] = floorPrice.value;
+    } else {
+      product['TCG Marketplace Price'] = +(product['TCG Market Price'] * marketPriceMultiplier.value).toFixed(2);
     }
   });
 };
 
-const updatePricing = () => {
-  pricing.value.forEach((product) => {
-    if (product['TCG Marketplace Price'] !== product['TCG Market Price']) {
-      if (floorPrice.value && product['TCG Market Price'] <= floorPrice.value) {
-        product['TCG Marketplace Price'] = floorPrice.value;
-      } else {
-        product['TCG Marketplace Price'] = product['TCG Market Price'] * marketPriceMultiplier.value;
-      }
-    }
-  });
-
+const exportPricing = () => {
   const sanitized = pricing.value.map((row) => {
     const sanitizedRow: Record<string, string | number> = {};
 
