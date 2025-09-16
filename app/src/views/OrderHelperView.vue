8b67fd4c281393278de2fa87581b7cafdf07cf84
Nathaniel Rupp <76455626+naroop@users.noreply.github.com>
2025-09-16 10:13:23 -0500

    add manage orders to order helper

--- app/src/views/OrderHelperView.vue
+++ app/src/views/OrderHelperView.vue
@@ -6,7 +6,47 @@
       </div>
 
       <div class="dark:border-surface-700 dark:bg-surface-900 rounded-md border border-gray-200 bg-white p-8 shadow">
-        <span class="text-2xl font-semibold">Order Helper</span>
+        <div class="flex items-center justify-between">
+          <span class="text-2xl font-semibold">Order Helper</span>
+
+          <Button label="Manage Orders" icon="pi pi-receipt" outlined :disabled="!shippingExport.length" @click="togglePopover" />
+          <Popover ref="manageOrders">
+            <div v-if="shippingExport.length" class="flex max-h-96 w-64 flex-col p-1">
+              <div class="dark:border-surface-700 text-muted-color border-b border-gray-300 text-sm">
+                <span>Orders</span>
+                <span class="float-right">{{ `(${shippingExport.length})` }}</span>
+              </div>
+              <ul class="overflow-y-auto">
+                <li v-for="order in shippingExport" :key="order['Order #']" class="flex items-center justify-between py-2">
+                  <span>{{ `${order.FirstName} ${order.LastName}` }}</span>
+                  <Button
+                    v-tooltip="'Remove'"
+                    text
+                    icon="pi pi-times"
+                    size="small"
+                    rounded
+                    severity="danger"
+                    @click="removeOrder(order['Order #'])"
+                  />
+                </li>
+              </ul>
+            </div>
+            <div v-else class="flex items-center">
+              <Button
+                icon="pi pi-undo"
+                label="Reset"
+                variant="text"
+                severity="secondary"
+                @click="
+                  () => {
+                    manageOrdersPopover?.hide();
+                    handleReset();
+                  }
+                "
+              />
+            </div>
+          </Popover>
+        </div>
 
         <router-view v-slot="{ Component }">
           <component
@@ -27,14 +67,19 @@
 <script setup lang="ts">
 import router from '@/router';
 import { parsePullSheetCsv, parseShippingCsv, type PullSheetCsv, type ShippingCsv } from '@/util/csv-parse';
-import { Button } from 'primevue';
-import { onMounted, ref } from 'vue';
+import { Button, Popover } from 'primevue';
+import { onMounted, ref, useTemplateRef } from 'vue';
 
 // Types ------------------------------------------------------------------------------
+interface OrderQuantity {
+  id: string;
+  quantity: number;
+}
 
 // Component Info (props/emits) -------------------------------------------------------
 
 // Template Refs ----------------------------------------------------------------------
+const manageOrdersPopover = useTemplateRef('manageOrders');
 
 // Variables --------------------------------------------------------------------------
 
@@ -77,6 +122,44 @@ const handleBack = () => {
 
 const handleReset = () => {
   router.push({ name: 'upload' });
+};
+
+const togglePopover = (event: Event) => {
+  manageOrdersPopover.value?.show(event);
+};
+
+const parseOrderQuantity = (orderQuantity: string): OrderQuantity[] => {
+  return orderQuantity
+    .split('|')
+    .map((s) => s.trim().split(':'))
+    .map(([id, quantity]) => ({ id, quantity: Number(quantity) }));
+};
+
+const unparseOrderQuantity = (orderQuantity: OrderQuantity[]): string => {
+  return orderQuantity.map(({ id, quantity }) => `${id}:${quantity}`).join(' | ');
+};
+
+const removeOrder = (orderNumber: string) => {
+  if (shippingExport.value.length) {
+    shippingExport.value = shippingExport.value.filter((row) => (row['Order #'] ?? '').trim() !== orderNumber);
+  }
+
+  pullSheet.value.forEach((pull) => {
+    const orderQuantity = parseOrderQuantity(pull['Order Quantity']);
+    let quantityRemoved = 0;
+    const updatedOrderQuantity = orderQuantity.filter(({ id, quantity }) => {
+      if (id !== orderNumber) {
+        return true;
+      } else {
+        quantityRemoved += quantity;
+        return false;
+      }
+    });
+    pull.Quantity -= quantityRemoved;
+    pull['Order Quantity'] = unparseOrderQuantity(updatedOrderQuantity);
+  });
+
+  pullSheet.value = pullSheet.value.filter((pull) => pull.Quantity > 0);
 };
 
 // Lifecycle Hooks --------------------------------------------------------------------
